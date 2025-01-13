d5d722c58753585afed7eaacc30212cd614d1ea5
ismaildrs <drissiisismail@gmail.com>
2025-01-13 19:13:53 +0100

    Adding some utils: swagger
    I included swagger to add documentation to the api

---
+++ src/utils/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const config = require("../config/env");
+
+
+// Configuration Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API des cours',
+            version: '1.0.0',
+            description: 'Documentation de l\'API des cours',
+        },
+        servers: [
+            {
+                url: `http://localhost:${config.port}/`,
+            },
+        ],
+    },
+    apis: ['./src/routes/*.js'], // Définissez où Swagger doit chercher les définitions des routes
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = { swaggerUi, swaggerDocs };
